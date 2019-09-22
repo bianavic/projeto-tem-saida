8f67f3c31e9f3a5b2eb3da2eac7a258459075cc1
Soares Israel <israeldiassoares@gmail.com>
2019-09-22 16:59:01 -0300

    criado layout da pagina principal

---
+++ components/Atoms/LogoPrefeitura.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <img src="../../assets/logos/logo-prefeitura-sp.jpg" alt="Logo Prefeitura de São Paulo" class="prefeitura-logo">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Logo-prefeitura-sp"
+    }
+</script>
+
+<style scoped>
+  .prefeitura-logo{
+    width: 120px;
+    height: 70px;
+  }
+</style>
---
+++ components/Molecules/LogoLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-header">
+        <div class="logo">
+            <nuxt-link to="javascript:void(0)">
+                <img src="../../assets/logos/logo-tem-saida-header.jpg" alt="Logo Tem Saida" class="tem-saida-logo">
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Logo'
+  }
+</script>
+
+<style scoped>
+    .tem-saida-logo {
+        width: 15vw;
+        height: 6vh;
+    }
+</style>
