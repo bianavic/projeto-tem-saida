ef9e2bfaa8c7aa02ed525b0e41b5818e87996c80
William Queiroz <wnqueiroz@outlook.com>
2019-10-15 21:13:58 -0300

    Configurando a factory de conexão ao MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from './modules/config.module';
+
+import { ConfigService } from './services/config.service';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DATABASE_MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
